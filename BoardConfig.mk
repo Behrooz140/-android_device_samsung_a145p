024e60f7da1ee8a04a0c1e3433c50213be190d48
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-08-03 01:51:19 +0330

    Refactor recovery init scripts addition with macro
    Refactored the addition of recovery init scripts to use a macro for safer file copying.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,14 +85,17 @@ BOARD_RECOVERY_RAMDISK_USE_SYSTEM_AS_ROOT := true
 # Recovery / File Systems
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
-# Init RC Scripts for Recovery
-PRODUCT_COPY_FILES += \
-    $(wildcard $(DEVICE_PATH)/Android.hardware.health-service.example_recovery.rc):recovery/root/Android.hardware.health-service.example_recovery.rc \
-    $(wildcard $(DEVICE_PATH)/dsms.rc):recovery/root/dsms.rc \
-    $(wildcard $(DEVICE_PATH)/libdsms.recovery.rc):recovery/root/libdsms.recovery.rc \
-    $(wildcard $(DEVICE_PATH)/mtk-plpath-utils.rc):recovery/root/mtk-plpath-utils.rc \
-    $(wildcard $(DEVICE_PATH)/servicemanager.recovery.rc):recovery/root/servicemanager.recovery.rc \
-    $(wildcard $(DEVICE_PATH)/snapuserd.rc):recovery/root/snapuserd.rc
+# Init RC Scripts for Recovery (Safe Copy)
+define add-rc-file
+$(if $(wildcard $(DEVICE_PATH)/$(1)),PRODUCT_COPY_FILES += $(DEVICE_PATH)/$(1):recovery/root/$(1))
+endef
+
+$(eval $(call add-rc-file,Android.hardware.health-service.example_recovery.rc))
+$(eval $(call add-rc-file,dsms.rc))
+$(eval $(call add-rc-file,libdsms.recovery.rc))
+$(eval $(call add-rc-file,​mtk-plpath-utils.rc))
+$(eval $(call add-rc-file,servicemanager.recovery.rc))
+$(eval $(call add-rc-file,snapuserd.rc))
 
 
 # USB / MTP / ADB Configuration
