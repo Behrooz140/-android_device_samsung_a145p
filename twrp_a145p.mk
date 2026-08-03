7f466d35b2f73f0d3f3d43c5fe87dd67de3e79a3
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-08-03 22:08:32 +0330

    Update twrp_a145p.mk

--- twrp_a145p.mk
+++ twrp_a145p.mk
@@ -14,3 +14,7 @@ PRODUCT_RELEASE_NAME := SM-A145P
 # Platform Version for Android 15 / One UI 7 (API 35)
 PLATFORM_VERSION := 15
 PLATFORM_VERSION_LAST_STABLE := 15
+
+# Copy MTK Preloader Path Utils & RC Files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/mtk-plpath-utils.rc:$(TARGET_COPY_OUT_RECOVERY)/root/mtk-plpath-utils.rc
