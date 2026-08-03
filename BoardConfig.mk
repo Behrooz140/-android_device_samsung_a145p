562d22eb8a11997be770ac1b420567348cc7772e
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-08-03 11:23:28 +0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 TWRP Device Tree for Samsung Galaxy A14 (SM-A145P)
+#
+
 DEVICE_PATH := device/samsung/a145p
 
 # For building with minimal manifest
@@ -19,46 +24,79 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := a14p
+TARGET_BOOTLOADER_BOARD_NAME := a145p
 TARGET_NO_BOOTLOADER := true
 
-# Fix 64-bit app support
-TARGET_SUPPORTS_64_BIT_APPS := true
+# Display
+TARGET_SCREEN_DENSITY := 450
+TARGET_USES_VULKAN := true
 
-# Prebuilt Kernel & DTB Configuration
-TARGET_NO_KERNEL := false
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Kernel
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 loop.max_part=7
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := Image
 
+# Kernel - Prebuilt (Files in root directory)
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/dtb
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/dtbo.img
-
-# Kernel Flags & Header Version
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_INCLUDE_DTBO := true
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/dtbo.img
+endif
 
-# Fix Vendor/Symlink Conflict for TWRP Ramdisk
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-BOARD_USES_GENERIC_KERNEL_IMAGE := true
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := false
-BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := false
 
 # Partitions
-BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_BOOTIMAGE_PARTITION_SIZE := 80740352
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 80740352
-BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_LARGE_FILESYSTEM := true
 
-# Dynamic Partitions (Samsung A145P MT6769V)
+# Dynamic / Super Partition Settings
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm system_ext
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product
+TARGET_COPY_OUT_VENDOR := vendor
 
-# Anti Rollback Patch
+# File Systems Configuration (Samsung Default)
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := erofs
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := erofs
+
+# Enable EROFS & F2FS Support in TWRP
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TW_INCLUDE_LPDUMP := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6769
+
+# Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+
+# Verified Boot (Bypass Samsung Anti-Rollback & Verification)
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+# Anti Rollback Hack
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
@@ -66,26 +104,11 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
-TW_INCLUDE_CRYPTO := false
-TW_INCLUDE_FASTBOOTD := true
-
-# Display & Screen Fixes
-TW_NO_SCREEN_TIMEOUT := true
-TW_NO_SCREEN_BLANK := true
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 
-# Fix vendor symlink crash during rsync packaging
-BOARD_BUILD_VENDOR_RAMDISK_IMAGE := false
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := false
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_RECOVERY_RAMDISK_USE_SYSTEM_AS_ROOT := true
-
-# Recovery / File Systems
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-
-# Init RC Scripts for Recovery (Safe Copy)
+# Init RC Scripts (Safe Copy)
 define add-rc-file
 $(if $(wildcard $(DEVICE_PATH)/$(1)),PRODUCT_COPY_FILES += $(DEVICE_PATH)/$(1):recovery/root/$(1))
 endef
@@ -93,27 +116,6 @@ endef
 $(eval $(call add-rc-file,Android.hardware.health-service.example_recovery.rc))
 $(eval $(call add-rc-file,dsms.rc))
 $(eval $(call add-rc-file,libdsms.recovery.rc))
-$(eval $(call add-rc-file,​mtk-plpath-utils.rc))
+$(eval $(call add-rc-file,mtk-plpath-utils.rc))
 $(eval $(call add-rc-file,servicemanager.recovery.rc))
 $(eval $(call add-rc-file,snapuserd.rc))
-
-
-# USB / MTP / ADB Configuration
-TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun%d/file"
-
-# TWRP USB IDs (Samsung / Standard Android)
-TW_EXCLUDE_SUPERSU := true
-TW_INCLUDE_CRYPTO := true
-TW_USE_TOOLBOX := true
-
-# ADB & MTP Enablement
-GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND_ONLY
-
-# USB Configfs Settings for Android 12
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXTRA_LANGUAGES := true
-
-
-
