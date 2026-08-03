c92f50e9e8d84d9bb8930685964c42f1adbe973e
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-08-03 17:10:44 +0330

    Update twrp_a145p.mk

--- twrp_a145p.mk
+++ twrp_a145p.mk
@@ -3,11 +3,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/a145p/BoardConfig.mk)
-
+# Product Specifications
 PRODUCT_DEVICE := a145p
 PRODUCT_NAME := twrp_a145p
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A145P
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := SM-A145P
+
+# Platform Version for Android 15 / One UI 7 (API 35)
+PLATFORM_SDK_VERSION := 35
+PLATFORM_VERSION := 15
+PLATFORM_VERSION_LAST_STABLE := 15
