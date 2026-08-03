0b72c69bb767801a161cb18b8cae8d9d0682755e
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-08-03 21:45:54 +0330

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 TWRP Device Tree for Samsung Galaxy A14 (SM-A145P)
+#
+
+export ALLOW_MISSING_DEPENDENCIES=true
+
+add_lunch_combo twrp_a145p-userdebug
+add_lunch_combo twrp_a145p-eng
